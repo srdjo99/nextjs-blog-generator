e9ff2eb7099a59d1e128af586e8e911dbe3db3ab
Srdjan <srdjancoralic5@gmail.com>
2023-04-27 10:26:31 +0200

    add layout basic structure and styling

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -4,10 +4,21 @@ type Props = {
   children?: any;
 };
 
-export const AppLayout = ({ children }: any) => {
+export const AppLayout: FC<Props> = ({ children }) => {
   return (
-    <div>
-      this is the app layout <div>{children}</div>
+    <div className='grid grid-cols-[300px_1fr] h-screen max-h-screen'>
+      <div className='flex flex-col overflow-hidden text-white'>
+        <div className='bg-slate-800'>
+          <div>logo</div>
+          <div>cta button</div>
+          <div>tokens</div>
+        </div>
+        <div className='flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800'>
+          list of posts
+        </div>
+        <div className='bg-cyan-800'>user info - logout</div>
+      </div>
+      <div className=''>{children}</div>
     </div>
   );
 };
