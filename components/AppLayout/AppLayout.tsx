237fb2d5e68a04fa4a8bd01e60f47e65fe5b11bb
Srdjan <srdjancoralic5@gmail.com>
2023-04-27 10:34:30 +0200

    add styled to user info in footer

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -1,10 +1,15 @@
 import { FC, ReactNode } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 type Props = {
-  children?: any;
+  children?: ReactNode;
 };
 
 export const AppLayout: FC<Props> = ({ children }) => {
+  const { user } = useUser();
+
   return (
     <div className='grid grid-cols-[300px_1fr] h-screen max-h-screen'>
       <div className='flex flex-col overflow-hidden text-white'>
@@ -16,7 +21,30 @@ export const AppLayout: FC<Props> = ({ children }) => {
         <div className='flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800'>
           list of posts
         </div>
-        <div className='bg-cyan-800'>user info - logout</div>
+        <div className='flex items-center h-20 gap-2 px-2 border-t bg-cyan-800 border-t-black/50'>
+          {!!user ? (
+            <>
+              <div className='min-w-[50px]'>
+                <Image
+                  priority
+                  src={user.picture as string}
+                  alt={user.name as string}
+                  height={50}
+                  width={50}
+                  className='rounded-full'
+                />
+              </div>
+              <div className='flex-1'>
+                <div className='font-bold'>{user.email}</div>
+                <Link className='text-sm' href='/api/auth/logout'>
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <Link href='/api/auth/login'>Login</Link>
+          )}
+        </div>
       </div>
       <div className=''>{children}</div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,8 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { useUser } from '@auth0/nextjs-auth0/client';
-
 export default function Home() {
-  const { user } = useUser();
-
-  console.log(user);
-
   return (
     <div>
       <h1>Homepage</h1>
-      <div>
-        {!!user ? (
-          <>
-            <div>
-              <Image
-                priority
-                src={user.picture as string}
-                alt={user.name as string}
-                height={50}
-                width={50}
-              />
-              <div>{user.email}</div>
-            </div>
-            <Link href='/api/auth/logout'>Logout</Link>
-          </>
-        ) : (
-          <Link href='/api/auth/login'>Login</Link>
-        )}
-      </div>
+      <div></div>
     </div>
   );
 }
