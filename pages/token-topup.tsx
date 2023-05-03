ea7db0e367867e5707cc664c2aa1b294e425e718
Srdjan <srdjancoralic5@gmail.com>
2023-05-03 21:54:17 +0200

    create token-topup page

--- pages/token-topup.tsx
+++ pages/token-topup.tsx
@@ -3,19 +3,39 @@ import { GetServerSideProps } from 'next';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { AppLayout } from '../components/AppLayout';
 import type { NextPageWithLayout } from './_app';
+import { getAppProps } from '../utils/getAppProps';
 
-const TokenTopup: NextPageWithLayout = () => {
-  return <div>topup post page</div>;
+const TokenTopup: any = () => {
+  const handleClick = async (e: any) => {
+    e.preventDefault();
+    const response = await fetch(`/api/addTokens`, {
+      method: 'POST',
+      headers: {},
+    });
+    const json = await response.json();
+    console.log(json, 'json');
+    window.location.href = json.session.url;
+  };
+
+  return (
+    <div>
+      <h1>this is the token topup</h1>
+      <button className='btn' onClick={handleClick}>
+        Add tokens
+      </button>
+    </div>
+  );
 };
 
-TokenTopup.getLayout = function getLayout(page: ReactElement) {
-  return <AppLayout>{page}</AppLayout>;
+TokenTopup.getLayout = function getLayout(page: any, pageProps: any) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
 };
 
 export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
   async getServerSideProps(ctx) {
+    const props = await getAppProps(ctx);
     return {
-      props: {},
+      props,
     };
   },
 });
